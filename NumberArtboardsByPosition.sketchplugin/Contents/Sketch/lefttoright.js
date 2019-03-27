f4622217c6f6befcdee8cf7eae290aaf81f50697
Richard Radermacher <richard.radermacher@gmail.com>
2019-03-27 17:55:12 +0100

    inital commit

---
+++ NumberArtboardsByPosition.sketchplugin/Contents/Sketch/lefttoright.js
@@ -0,0 +1,49 @@
+var onRun = function(context) {
+    // reference the sketch document
+    var doc = context.document;
+    var selection = context.selection;
+    var numArtboard;
+
+    // check if user selected something
+    if (selection.count() == 0) {
+        doc.showMessage("Please select artboards.");
+    }
+
+    // check if every layer selected is an Artboard
+    var selectedArtboards = selection.every(function(layer){
+        return layer.isMemberOfClass(MSArtboardGroup)
+    });
+
+    if (!selectedArtboards) {
+        doc.showMessage("Only artboards allowed. Please only select artboards!");
+    } else {
+        var _selection = [];
+        selection.forEach(function (artboard, i) {
+            _selection.push(artboard);
+        });
+        _selection.sort(function(a, b) {
+            var value = a.rect().origin.y - b.rect().origin.y;
+            return value;
+        });
+        _selection.sort(function(a, b) {
+            var value = a.rect().origin.x - b.rect().origin.x;
+            return value;
+        });
+        numArtboard = _selection.length;
+        // loop through the selected artboards
+        for (var i = 0; i < numArtboard; i++) {
+            // reference the selection
+            var artboard = _selection[i];
+            
+            // remove last number from string
+            var nameParts = artboard.name().split(" ");
+            if( Number.isInteger(nameParts[nameParts.length-1] * 1) ) {
+                nameParts.splice(nameParts.length-1, 1);
+            }
+            var artboardName = nameParts.join(" ");
+
+            var newArtboardName = artboardName + " " + (i+1);
+            artboard.setName(newArtboardName);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# sketch-number-artboards-by-position
+
+## How does it work?
+1. Place artboards in order on canvas
+2. Select artboards
+3. Press `command shift →` or `command shift ←` or `command shift ↓` or `command shift ↑`
+
+--
+Inspired by [mmasonlee](https://github.com/mmasonlee/sketch-number-artboards) plugin
